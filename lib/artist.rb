a479cbcdb62460074999a2f3293ae2d179ef1357
zman613 <monthlyschnitzel@gmail.com>
2019-11-20 14:48:44 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+
+    attr_accessor :name
+    attr_reader
+
+    def initialize
+        @name
+    end
+
+end
---
+++ lib/author.rb
@@ -0,0 +1,9 @@
+class Author
+
+    attr_accessor :name
+    attr_reader
+
+    def initialize
+        @name
+    end
+end
