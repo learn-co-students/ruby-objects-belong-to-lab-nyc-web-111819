d8b80a2946a43b3266560d226ce5375f5533a0b1
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-20 13:43:07 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,15 @@
+class Post
+    attr_accessor :title
+
+    def initialize
+        @title = title
+    end
+
+    def author=(author)
+        @author = author
+    end
+
+    def author
+        @author
+    end
+end
